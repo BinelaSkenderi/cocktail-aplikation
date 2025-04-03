83a38cb4006907f6d1b6e41913e66424f447090b
binela <binela.skenderi@outlook.com>
2025-04-03 23:04:22 +0200

    fixat lite korterna snygat lite

--- src/components/ui/categoryCard.tsx
+++ src/components/ui/categoryCard.tsx
@@ -21,29 +21,34 @@ const DrinkCard: React.FC<DrinkCardProps> = ({ name, link, id }) => {
         width: "200px",
         height: "300px",
         border: "16px solid", // added border style for better visibility
+        borderRadius:"8px",
         padding: "16px",
         justifyContent: "space-between",
+        boxShadow: "0px 4px 6px", 
+        overflow:"hidden",
       }}
     >
       <div
-        style={{
+        style={{flex: "1"
           //   width: "100%",
           //   height: "100%",
-        }}
+}}
       >
         <img
           style={{
             width: "100%",
             height: "100%",
+            objectFit: "cover",
+            borderRadius: "4px"
           }}
           src={link}
           alt={`${name} img`}
         />
       </div>
 
-      <div style={{ padding: "8px" }}>
-        <h4 style={{ margin: "0" }}>{name}</h4>
-        <p style={{ margin: "0" }}>ID: {id}</p>
+      <div style={{ padding: "2px",}}>
+        <h4 style={{ fontSize: "14px", margin: "0px" }}>{name}</h4>
+        <p style={{ margin: "0px", fontSize:"11px" }}>ID: {id}</p>
         <button onClick={goVisit}>Details</button> {/* standard button element */}
       </div>
     </div>
